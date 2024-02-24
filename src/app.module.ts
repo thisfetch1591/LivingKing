82000734a44c1924782b03387da238884f4311ab
thisfetch1591 <thisfetch1591@gmail.com>
2024-02-25 03:01:57 +0900

    refactor: domain 별 controller, service 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostController } from './post/post.controller';
+import { UserController } from './user/user.controller';
+import { CommentController } from './comment/comment.controller';
+import { PostService } from './post/post.service';
+import { UserService } from './user/user.service';
+import { CommentService } from './comment/comment.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    PostController,
+    UserController,
+    CommentController,
+  ],
+  providers: [AppService, PostService, UserService, CommentService],
 })
 export class AppModule {}
